83e89b34de6d054e7f96022b79e3638e83a17fb9
Yiyten <yiyten390@gmail.com>
2023-09-08 09:17:06 -0700

    ,mkjj

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,24 +16,21 @@ int main(void)
 		secondDigit = firstDigit;
 		while (secondDigit <= 99)
 		{
-			if (secondDigit != firstDigit)
+			if (seconDigit != firstDigit)
 			{
 				putchar((firstDigit / 10) + 48);
 				putchar((firstDigit % 10) + 48);
-				putchar(',');
 				putchar(' ');
 				putchar((secondDigit / 10) + 48);
 				putchar((secondDigit % 10) + 48);
-				putchar(';');
-				putchar(' ');
 
-				if (firstDigit != 98 || secondDigit != 99)
+				if (firstDigit != 98 || seconDigit != 99)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			secondDigit++;
+			seconDigit++;
 		}
 		firstDigit++;
 	}

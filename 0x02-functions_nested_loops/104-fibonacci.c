a976558361c657a12811bbd532e77b13b7dc05af
Yiyten <yiyten390@gmail.com>
2023-09-13 14:08:54 -0700

    mooo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,19 +32,19 @@ int numLength(int num)
 
 int main(void)
 {
-	int count, intitial0s;
+	int count, initialOs;
 	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
+		initialOs = numLength(mx) - 1 - numLength(f1);
 
-		while (f1o > 0 && initial0s > 0)
+		while (f1o > 0 && initialOs > 0)
 		{
 			printf("%d", 0);
-			initial0s--;
+			initialOs--;
 		}
 
 		printf("%lu", f1);

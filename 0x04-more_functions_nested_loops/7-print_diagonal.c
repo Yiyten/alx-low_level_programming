eaa2264ba9003901f679301b2edd0e1e87c524f7
Yiyten <yiyten390@gmail.com>
2023-09-14 13:50:52 -0700

    mozz

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,16 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
-
-	if (n <= 0)
-		_putchar('\n');
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0 ; j <= i ; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
+	for (int i = 0; i < n; i++)
+	{
+	for (int j = 0; j <= i; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+	}
+	if (n <= 0) {
+	_putchar('\n');
+	}
 }

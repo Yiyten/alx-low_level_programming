407ea34f110f79fd48c5ae121f1826b528338829
Yiyten <yiyten390@gmail.com>
2023-09-14 12:30:21 -0700

    mozz

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

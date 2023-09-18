b22819a831c6b3d1b78e4bb56f98f849651dd7df
Yiyten <yiyten390@gmail.com>
2023-09-18 14:08:03 -0700

    meko

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * 8dest: char type string
+ * 8src:char type string
+ * Description:copy the string
+ * the buffer pointed to by "dest"
+ * Return: pointer to "dest"
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}

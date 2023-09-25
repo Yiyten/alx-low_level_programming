f3830c6226743c7300b854cc2382df8225b973e0
Yiyten <yiyten390@gmail.com>
2023-09-25 05:59:38 -0700

    new

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sete the value of pointer to chat
+ * @s: pointer to pointer
+ * @to: pointer char.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

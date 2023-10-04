9e413fc72bc20c23f8f51dace28c9f9c21fb5b1a
Yiyten <yiyten390@gmail.com>
2023-10-04 13:26:17 -0700

    thy

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
  *main-prints the min num of coins
  *to make change for an amount of money
@@ -10,29 +12,33 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-	int i, leastcents = 0, money = atoi(argv[1]);
-	int cents[] = {25, 10, 5, 2, 1};
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	for (i = 0; i < 5; i++)
-	{
-		if (money >= cents[i])
-		{
-			leastcents += money / cents[i];
-			money = money % cents[i];
-			if (money % cents[i] == 0)
-			{
-				break;
-			}
-		}
-	}
-	printf("%d\n", leastcents);
-	}
-	else
+	if (argc !== 2)
 	{
 		printf("Error\n");
-		return (i);
+		return (1);
 	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
 	return (0);
 }
